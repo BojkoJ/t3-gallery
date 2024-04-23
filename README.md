2c53996ca5fd616ba1fe020fb4d70f4650023af8
Jan Bojko <honzabojko@seznam.cz>
2024-04-23 22:47:41 +0200

    Model update in Drizzle ORM, Connect DB to UI

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 
 - [x] Make it deploy (Vercel)
 - [x] Scaffold basic ui with mock data
-- [ ] Actually set up a database (Vercel Postgres)
-- [ ] Attach database to UI
+- [x] Actually set up a database (Vercel Postgres)
+- [x] Attach database to UI
 - [ ] Add authentication (w/ Clerk)
 - [ ] Add image upload
 - [ ] Error management (w/ Sentry)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,30 +2,18 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/7d53712b-ec35-4b9d-a3d1-655280fa34bb-34z34k.webp",
-  "https://utfs.io/f/cfc3e9f2-fbdb-4406-9f06-28a0b83660aa-34n4a4.png",
-  "https://utfs.io/f/52d6c121-ce48-4683-813d-108a190242bf-h86lnw.png",
-];
-
-const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }));
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { asc }) => asc(model.id),
+  });
 
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div className="" key={post.id}>
-            {post.name}
-          </div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+        {[...images, ...images, ...images].map((image, index) => (
           <div className="w-48" key={image.id + "-" + index}>
             <img src={image.url} alt="image" />
+            <div className="">{image.name}</div>
           </div>
         ))}
       </div>
