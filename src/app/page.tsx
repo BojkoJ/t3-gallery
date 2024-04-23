20a633a1a686c57404c9b52ca8fdd80600e8f7e2
Jan Bojko <honzabojko@seznam.cz>
2024-04-23 21:28:15 +0200

    Database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const mockUrls = [
   "https://utfs.io/f/7d53712b-ec35-4b9d-a3d1-655280fa34bb-34z34k.webp",
   "https://utfs.io/f/cfc3e9f2-fbdb-4406-9f06-28a0b83660aa-34n4a4.png",
@@ -6,12 +8,21 @@ const mockUrls = [
 
 const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div className="w-48" key={image.id}>
+        {posts.map((post) => (
+          <div className="" key={post.id}>
+            {post.name}
+          </div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div className="w-48" key={image.id + "-" + index}>
             <img src={image.url} alt="image" />
           </div>
         ))}
