59e231b6d4de0b75e4664b1123307d411809284b
Jan Bojko <honzabojko@seznam.cz>
2024-08-18 21:14:51 +0200

    posthog adblocker prevent

--- next.config.js
+++ next.config.js
@@ -15,6 +15,22 @@ const coreConfig = {
   },
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+      {
+        source: "/ingest/decide",
+        destination: "https://us.i.posthog.com/decide",
+      },
+    ];
+  },
 };
 
 import { withSentryConfig } from "@sentry/nextjs";
