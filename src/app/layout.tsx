bd5e2bfcde62e7ff38ac8a86185ceaff6d404db9
Jan Bojko <honzabojko@seznam.cz>
2024-08-18 12:31:04 +0200

    uploadThing settup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import "~/styles/globals.css";
+import "@uploadthing/react/styles.css";
 
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+
 import TopNav from "~/components/TopNav";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "./api/uploadthing/core";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -10,9 +15,9 @@ const inter = Inter({
 });
 
 export const metadata = {
-  title: "T3 Gallery",
-  description: "T3 Stack Web app - Gallery",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  title: "Gallery",
+  description: "Web app - Gallery",
+  icons: [{ rel: "icon", url: "/icon.svg" }],
 };
 
 export default function RootLayout({
@@ -22,6 +27,15 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
+      <NextSSRPlugin
+        /**
+         * The `extractRouterConfig` will extract **only** the route configs
+         * from the router to prevent additional information from being
+         * leaked to the client. The data passed to the client is the same
+         * as if you were to fetch `/api/uploadthing` directly.
+         */
+        routerConfig={extractRouterConfig(ourFileRouter)}
+      />
       <html lang="en">
         <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
           <TopNav />
--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,14 +1,28 @@
+"use client";
+
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { UploadButton } from "~/utils/uploadthing";
 
 const TopNav = () => {
+  const router = useRouter();
+
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
-      <div>
+      <div className="flex flex-row">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={() => {
+              router.refresh();
+              // re-runs the current route you are on, on the server
+              // and sends you the neccessary parts to update the pages content
+            }}
+          />
           <UserButton />
         </SignedIn>
       </div>
